1eb877c58fcaa016bf547ca32b083dca03f381f8
Kelsey Maes <kelsey@vpprocess.com>
2018-03-29 11:23:24 -0700

    Limit PI-SPI-DIN 8AI SPI frequency to 100kHz

--- src/pi_spi_din_8ai.c
+++ src/pi_spi_din_8ai.c
@@ -1,8 +1,8 @@
 #include <spi.h>
-#include <wiringPi.h> 
-//#include <wiringPiSPI.h>
 
 #include "pi_spi_din.h"
+
+#define MAX_SPI_FREQ	100000
 
 uint16_t pi_spi_din_8ai_read_single(enum chip_enable ce, uint8_t channel)
 {
@@ -10,16 +10,12 @@ uint16_t pi_spi_din_8ai_read_single(enum chip_enable ce, uint8_t channel)
 	
 	uint8_t data[3] =
 	{
-		0x06 | (channel >> 2 & 0x01),
-		channel << 6 & 0xC0,
+		0x06 | ((channel >> 2) & 0x01),
+		(channel << 6) & 0xC0,
 		0
 	};
 	
-	/*digitalWrite(chip_enable, LOW);
-	wiringPiSPIDataRW(0, data, 3);
-	digitalWrite(chip_enable, HIGH);*/
-	
-	spi_open(ce, 500000);
+	spi_open(ce, MAX_SPI_FREQ);
 	spi_transfer(data, 3);
 	
 	return (((uint16_t)data[1] & 0x0F) << 8) | data[2];
