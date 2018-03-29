5712695cf01485cf6d407156079c5839c7cb6748
Kelsey Maes <kelsey@vpprocess.com>
2018-03-29 13:16:36 -0700

    Fix Steinhart-hart and rage function Python bindings. Move to 1.0.2

--- python/widgetlords/__init__.py
+++ python/widgetlords/__init__.py
@@ -4,6 +4,12 @@ __all__ = []
 
 widgetlords = cdll.LoadLibrary('libwidgetlords.so')
 
+widgetlords.widgetlords_steinhart_hart.argtypes = [c_double, c_double, c_ushort, c_ushort]
+widgetlords.widgetlords_steinhart_hart.restype = c_double
+
+widgetlords.widgetlords_counts_to_value.argtypes = [c_ushort, c_ushort, c_ushort, c_double, c_double]
+widgetlords.widgetlords_counts_to_value.restype = c_double
+
 def steinhart_hart(r_room: float, r_beta: float, counts_range: int, counts: int):
     return widgetlords.widgetlords_steinhart_hart(r_room, r_beta, counts_range, counts)
 __all__.append('steinhart_hart')
