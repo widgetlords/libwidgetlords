184feb0e5a86ffb2e8372438a90998fff9790125
Kelsey Maes <kelsey@vpprocess.com>
2019-09-06 09:17:08 -0700

    Improve overlays. PI-SPI series now has optional extra slave selects and the ability to reassign existing ones to avoid conflicts.

--- overlays/overlay.sh
+++ overlays/overlay.sh
@@ -4,4 +4,3 @@ dtc -@ -I dts -O dtb -o pi-spi.dtbo pi-spi.dts
 dtc -@ -I dts -O dtb -o vpe-2701c.dtbo vpe-2701c.dts
 dtc -@ -I dts -O dtb -o vpe-2901a.dtbo vpe-2901a.dts
 dtc -@ -I dts -O dtb -o vpe-3011b.dtbo vpe-3011b.dts
-#install *.dtbo /boot/overlays
